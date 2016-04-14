3ff68622fa23b1607b4ac9555538a356b74d0a45
Arthur Lu <arthur.lv6@gmail.com>
2016-04-14 22:52:26 +1200

    missed files added.

---
+++ Native/PhoneApp/PhoneApp/ViewModels/ContactViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+namespace PhoneApp
+{
+	public class ContactViewModel:INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public String URL{ get; set;}
+		public ContactViewModel ()
+		{
+			WebViewShow = false;
+			IndicatorShow = true;
+		}
+		private bool webViewShow;
+		public bool WebViewShow { get{ return webViewShow; } set{
+				if (webViewShow != value)
+				{
+					webViewShow = value;
+					OnPropertyChanged("WebViewShow");
+				}
+			}
+		}
+		private bool indicatorShow;
+		public bool IndicatorShow { get{ return indicatorShow; } set{
+				if (indicatorShow != value)
+				{
+					indicatorShow = value;
+					OnPropertyChanged("IndicatorShow");
+				}
+			}
+		}
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this,
+					new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
+
---
+++ Native/PhoneApp/PhoneApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using PhoneApp.Helpers;
+using PhoneApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PhoneApp.ViewModels
+{
+    public class HomeViewModel
+    {
+		public async Task<List<Advertisement>> Ads(string category=null,string search="none")
+        {
+			var categoryId = 0;
+			switch (category) {
+			case "All":
+				categoryId = 0;
+				break;
+			case "Companies":
+				categoryId = 2;
+				break;
+			case "Recruits":
+				categoryId = 4;
+				break;
+			case "RentRooms":
+				categoryId = 6;
+				break;
+			case "Products":
+				categoryId = 7;
+				break;
+			case "PersonalInfo":
+				categoryId = 18;
+				break;
+			default:
+				categoryId = 0;
+				break;
+			}
+            var client = ADHttpClient.GetClient();
+			var egsResponse = await client.GetAsync("api/category/"+categoryId.ToString()+"/advertisements?search="+search+"&fields=id,keyword,shortdescription,categoryid");
+            if (egsResponse.IsSuccessStatusCode)
+            {
+                string egsContent = await egsResponse.Content.ReadAsStringAsync();
+                var lst = JsonConvert
+                    .DeserializeObject<List<Advertisement>>(egsContent);
+                return lst;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
